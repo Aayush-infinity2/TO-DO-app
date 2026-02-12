8cc017cf79dc5cdab6180ccc231015f1316b05cf
Aayush@infinity <96738534+Aayush-infinity2@users.noreply.github.com>
2026-02-12 10:36:40 +0530

    Refactor URL patterns and update include path

--- DB_integration/app/urls.py
+++ DB_integration/app/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('', include('todo.urls')),
+    path('', include('ToDo.urls')),
     path('admin/', admin.site.urls),
 
 ]
+
 
